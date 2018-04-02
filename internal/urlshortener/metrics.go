cc0badb683e47f1953729f311704f5ecad8f70ed
Fabian Selles Rosa <fabian.selles@schibsted.com>
2018-04-02 12:05:27 +0200

    first zipkin integration

--- internal/urlshortener/metrics.go
+++ internal/urlshortener/metrics.go
@@ -1,6 +1,7 @@
 package urlshortener
 
 import (
+	"context"
 	"time"
 
 	"github.com/go-kit/kit/metrics"
@@ -22,29 +23,29 @@ type metricsMiddleware struct {
 	requestLatency metrics.Histogram
 }
 
-func (mw *metricsMiddleware) Shortify(longURL string) (mapping *shortURL, err error) {
+func (mw *metricsMiddleware) Shortify(ctx context.Context, longURL string) (mapping *shortURL, err error) {
 	defer func(begin time.Time) {
 		lvs := []string{"method", "Shortify"}
 		mw.requestCount.With(lvs...).Add(1)
 		mw.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
 	}(time.Now())
-	return mw.Service.Shortify(longURL)
+	return mw.Service.Shortify(ctx, longURL)
 }
 
-func (mw *metricsMiddleware) Resolve(shortURL string) (mapping *shortURL, err error) {
+func (mw *metricsMiddleware) Resolve(ctx context.Context, shortURL string) (mapping *shortURL, err error) {
 	defer func(begin time.Time) {
 		lvs := []string{"method", "Resolve"}
 		mw.requestCount.With(lvs...).Add(1)
 		mw.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
 	}(time.Now())
-	return mw.Service.Resolve(shortURL)
+	return mw.Service.Resolve(ctx, shortURL)
 }
 
-func (mw *metricsMiddleware) GetInfo(shortURL string) (mapping *shortURL, err error) {
+func (mw *metricsMiddleware) GetInfo(ctx context.Context, shortURL string) (mapping *shortURL, err error) {
 	defer func(begin time.Time) {
 		lvs := []string{"method", "Info"}
 		mw.requestCount.With(lvs...).Add(1)
 		mw.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
 	}(time.Now())
-	return mw.Service.GetInfo(shortURL)
+	return mw.Service.GetInfo(ctx, shortURL)
 }
